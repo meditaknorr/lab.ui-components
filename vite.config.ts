bf412cbaafbf18374a4f134f43811e526ee7e5dc
Medita Knorr <51007331+meditaknorr@users.noreply.github.com>
2023-02-11 01:39:17 +0200

    Project Init [2]

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsconfigPaths(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+      typescript: true,
+    }),
+    react(),
+  ],
+  build: {
+    outDir: path.join(__dirname, 'dist/build'),
+  },
+});
